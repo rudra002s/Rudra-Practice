b31c5ba94f942718ad7dbf59d346f60ead1ca124
Rudra Sharma <rudras.1902@gmail.com>
2026-01-21 00:19:46 +0530

    first commit

---
+++ basics/script.js
@@ -0,0 +1,16 @@
+alert("Hello world")
+
+console.log("code is running...")
+console.log("code is also running...")
+
+var a = prompt("Enter your number")
+var isTrue = confirm("are you sure to enter this page")
+
+if(isTrue){
+    console.log("Entering page")
+}
+
+else{
+    console.log("Not entering to the page")
+}
+console.log("Your number is:" +a)
