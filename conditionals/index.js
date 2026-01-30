937f9a3914533e354903bc6a8f5a9fd87d09d5be
Rudra Sharma <rudras.1902@gmail.com>
2026-01-30 15:01:51 +0530

    feat: add conditional logic  index.js

--- conditionals/index.js
+++ conditionals/index.js
@@ -1,1 +1,18 @@
 console.log("Hello i am rudra")
+
+let age=7;
+let grace=2;
+
+console.log(age+grace)
+console.log(age-grace)
+console.log(age*grace)
+console.log(age/grace)
+console.log(age**grace)
+
+if((age+grace)>18){
+    console.log("you can drive");
+}
+
+else{
+    console.log("you cannot drive");
+}
