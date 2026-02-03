0eb3efe7b4173599c3d57974dc4d4f91a7f5eac0
Rudra Sharma <rudras.1902@gmail.com>
2026-02-03 11:58:37 +0530

    add object to map operations for faulty calculator

--- FAULTY CALC/index.js
+++ FAULTY CALC/index.js
@@ -17,9 +17,17 @@ let a=prompt("Enter first number");
 let b=prompt("Enter second number");
 let c=prompt("Enter operation");
 
+let obj={
+    "+":"-",
+    "*":"+",
+    "-":"/",
+    "/":"**"
+}
+
 if(random>0.1){
     //for correct calculations
 }
+
 else{
     //for wrong calculations
 }
