b666f0348cfe23237d02fe0ed0124bd8e6956114
Rudra Sharma <rudras.1902@gmail.com>
2026-02-03 11:51:19 +0530

    implement user input prompts and random operation logic for faulty calculator

--- FAULTY CALC/index.js
+++ FAULTY CALC/index.js
@@ -10,3 +10,16 @@
 it performs wrong operation 10% of the times
 
  */
+
+let random=Math.random()
+
+let a=prompt("Enter first number");
+let b=prompt("Enter second number");
+let c=prompt("Enter operation");
+
+if(random>0.1){
+    //for correct calculations
+}
+else{
+    //for wrong calculations
+}
