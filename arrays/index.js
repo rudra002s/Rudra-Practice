7fb726976a52d0302204a40c46185c8425201a70
Rudra Sharma <rudras.1902@gmail.com>
2026-02-05 14:10:19 +0530

    initial array declaration and logging in index.js

---
+++ arrays/index.js
@@ -0,0 +1,6 @@
+//array is also declared as a variable only with [] and array are mutable which means when we perform a perform original array is changed but in strings when we perform a function it returns a new string and original string is intact
+
+let arr=[1,2,3,4,5]
+
+console.log(arr)
+console.log(arr.length) //to print length of array
