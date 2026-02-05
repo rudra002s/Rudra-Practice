8f34b68d1b51d5e7030e0733507adf39b628bb60
Rudra Sharma <rudras.1902@gmail.com>
2026-02-05 19:42:29 +0530

    learnt about loops in array

---
+++ arrays/loops.js
@@ -0,0 +1,27 @@
+let arr = [1, 24, 5, 6, 90]
+
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     console.log(element)
+// } //can also be used for traversing an array
+
+// arr.forEach((value,index,arr) => {
+//     console.log(value,index,arr)
+// }); //used to get vale its index then full array
+
+// let obj = {
+//     a: 1,
+//     b: 2,
+//     c: 3
+// }
+// for (const key in obj) {
+//     if (!Object.hasOwn(obj, key)) continue;
+
+//     const element = obj[key];
+//     console.log(key, element)
+
+// }
+
+// for (const element of arr) {
+//     console.log(element)
+// }
