9741bf497b0fdc0e56f2a7636907cc24015949e7
Rudra Sharma <rudras.1902@gmail.com>
2026-02-05 23:58:00 +0530

    comment out unused console logs and add new DOM element logging

--- DOM/script.js
+++ DOM/script.js
@@ -9,8 +9,12 @@ console.log(document.body)
 
 // console.log(document.body.childNodes[1].firstElementChild.style.backgroundColor="yellow") //used css properties as well
 
-console.log(document.body.firstElementChild)
-console.log(document.body.firstElementChild.children[0])
-console.log(document.body.firstElementChild.children[1])
-console.log(document.body.firstElementChild.children[1].nextElementSibling)
-console.log(document.body.firstElementChild.children[1].previousElementSibling)
+// console.log(document.body.firstElementChild)
+// console.log(document.body.firstElementChild.children[0])
+// console.log(document.body.firstElementChild.children[1])
+// console.log(document.body.firstElementChild.children[1].nextElementSibling)
+// console.log(document.body.firstElementChild.children[1].previousElementSibling)
+
+
+console.log(document.body.children)
+console.log(document.body.children[1].rows)
