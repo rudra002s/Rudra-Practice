f8f4b7ca9b95d6a88cd64fed91b2a5beaf99bf53
Rudra Sharma <rudras.1902@gmail.com>
2026-02-05 14:13:48 +0530

    enhance array element logging and clarify index usage in index.js

--- arrays/index.js
+++ arrays/index.js
@@ -1,6 +1,15 @@
 //array is also declared as a variable only with [] and array are mutable which means when we perform a perform original array is changed but in strings when we perform a function it returns a new string and original string is intact
 
 let arr=[1,2,3,4,5]
+// index=0,1,2,3,4
 
 console.log(arr)
 console.log(arr.length) //to print length of array
+
+
+console.log(arr[0]) //to print a particular element of an array using its index
+console.log(arr[1])
+console.log(arr[2])
+console.log(arr[3])
+
+arr[0]=555; //here it changed the element of array
