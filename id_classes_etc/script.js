925d7c02ae0d447d77b28659081b8092a6f6f6b8
Rudra Sharma <rudras.1902@gmail.com>
2026-02-06 09:41:53 +0530

    update index.html to add a new box element and modify script.js for background color styling

--- id_classes_etc/script.js
+++ id_classes_etc/script.js
@@ -1,6 +1,8 @@
 console.log("Rudra can do it")
 
-let boxes = document.getElementsByClassName("box")
-console.log(boxes)
+// let boxes = document.getElementsByClassName("box")
+// // console.log(boxes)
 
-boxes[2].style.backgroundColor = "yellow"
+// boxes[2].style.backgroundColor = "yellow"
+
+document.getElementById("red").style.backgroundColor="yellow"
