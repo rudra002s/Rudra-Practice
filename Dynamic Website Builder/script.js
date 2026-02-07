ba6b31a811a46952ae1de8d27807b26b1c18dfcd
Rudra Sharma <rudras.1902@gmail.com>
2026-02-07 10:02:30 +0530

    Fix HTML structure in createCard function for proper rendering

--- Dynamic Website Builder/script.js
+++ Dynamic Website Builder/script.js
@@ -1,13 +1,14 @@
 function createCard(title,channelName,views,monthsOld,duration,thumbnail){
     let HTML=`
     <div class="card">
-            <h3="${title}>
-            <p="${channelName}>
-            <p="${views}>
-            <0="${monthsOld}>
-            <span="${duration}>
-            <img source="${thumbnail}>
+            <h3>"${title}</h3>
+            <p>${channelName}</p>
+            <p>${views}</p>
+            <p>${monthsOld}</p>
+            <span>${duration}</span>
+            <img src="${thumbnail}>
         </div>
     `
+    document.querySelector(".container").insertAdjacentHTML("beforeend",HTML)
 }
 createCard("Installing VS Code & How Website Works | Sigma Web Development Course - Tutorial #1","CodeWithHarry","6.8M","September 26, 2023","31:19","https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDf-xrqgUw55JSfKsoykKVhngkzRA")
