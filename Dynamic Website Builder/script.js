dc0f0758e7072bea82020c54e1460e0004313953
Rudra Sharma <rudras.1902@gmail.com>
2026-02-07 10:49:51 +0530

    Add capsule element to card component for video duration display and style adjustments

--- Dynamic Website Builder/script.js
+++ Dynamic Website Builder/script.js
@@ -3,8 +3,8 @@ function createCard(title, channelName, views, monthsOld, duration, thumbnail) {
     <div class="card">
         <div class="img">
           <img
-            src="https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDf-xrqgUw55JSfKsoykKVhngkzRA"
-          />
+            src="${thumbnail}"/>
+            <div class="capsule">${duration}</div>
         </div>
         <div class="text">
           <h1>
@@ -14,5 +14,6 @@ function createCard(title, channelName, views, monthsOld, duration, thumbnail) {
         </div>
       </div>
     `
+    document.querySelector(".container").innerHTML=document.querySelector(".container").innerHTML+HTML
 }
 createCard("Installing VS Code & How Website Works | Sigma Web Development Course - Tutorial #1", "CodeWithHarry", "6.8M", "September 26, 2023", "31:19", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDf-xrqgUw55JSfKsoykKVhngkzRA")
