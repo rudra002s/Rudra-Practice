2932d46b96a850e250a3a9e24000960c46ada237
Rudra Sharma <rudras.1902@gmail.com>
2026-02-07 09:47:58 +0530

    Add example usage for createCard function in script.js

--- Dynamic Website Builder/script.js
+++ Dynamic Website Builder/script.js
@@ -1,3 +1,4 @@
 function createCard(title,channelName,views,monthsOld,duration,thumbnail){
 
 }
+createCard("Installing VS Code & How Website Works | Sigma Web Development Course - Tutorial #1","CodeWithHarry","6.8M","September 26, 2023","31:19","https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDf-xrqgUw55JSfKsoykKVhngkzRA")
